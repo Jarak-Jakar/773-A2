1f5a0f7cb90767597edc3f4a91c69fe768595fe6
James Cooper <jarak_jakar@hotmail.com>
2016-03-13 21:39:06 +1300

    Further work on parsing values
    And trying to calculate Xd and Yd

--- A2.cpp
+++ A2.cpp
@@ -14,7 +14,15 @@ void main(int argc, char *argv[])
     if(pointsFile.is_open()) {
         string line;      
         getline(pointsFile, line);
-        int totalLines = stoi(line);      
+		int totalLines = stoi(line.substr(0, line.find(",")));
+		line.erase(0, (line.find(",") + 1));
+		double nummerA = stod(line.substr(0, line.find(",")));
+		line.erase(0, (line.find(",") + 1));
+		double nummerB = stod(line.substr(0, line.find(",")));
+		line.erase(0, (line.find(",") + 1));
+		double pixelSize = stod(line.substr(0, line.find(",")));
+		//line.erase(0, (line.find(",") + 1));
+		cout << "pixelSize = " << pixelSize << endl;
         vector<vector<double>> inputPoints(totalLines);      
         for(int i = 0; i < totalLines; i++) {
             getline(pointsFile, line);
@@ -27,7 +35,44 @@ void main(int argc, char *argv[])
             }
             inputPoints.push_back(tempArray);
         }
-        //cout << inputPoints.front().front() <<endl;
+
+		/*vector<vector<double>>::iterator outerIter;
+		outerIter = inputPoints.begin();
+
+		for (; outerIter != inputPoints.end(); outerIter++)
+		{
+			vector<double> tempArray;
+			tempArray = *outerIter;
+			vector<double>::iterator innerIter;
+			innerIter = tempArray.begin();
+			for (; innerIter != tempArray.end(); innerIter++)
+			{
+				cout << "Element: " << *innerIter << endl;
+			}
+
+			cout << "End of entry" << endl << endl;
+		}*/
+
+		vector<vector<double>>::iterator grabXnY;
+		cout << "Declared grab" << endl;
+		grabXnY = inputPoints.begin();
+		cout << "Initialised grab" << endl;
+		for (; grabXnY != inputPoints.end(); grabXnY++)
+		{
+			cout << "Entered for loop" << endl;
+			vector<double> tempArray = *grabXnY;
+			cout << "Dereferenced grab" << endl;
+			double temp;
+			double Xd; // = tempArray[3] * pixelSize;
+			temp = tempArray[3];
+			Xd = temp * pixelSize;
+			cout << "Made Xd" << endl;
+			double Yd; // = tempArray[4] * pixelSize;
+			temp = tempArray[4];
+			Yd = temp * pixelSize;
+			cout << "Made Yd" << endl;
+			cout << "Xd & Yd are: " << Xd << ", " << Yd << endl;
+		}
     }
     pointsFile.close();
 }
