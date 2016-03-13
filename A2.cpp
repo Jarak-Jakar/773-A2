1653d047e96610f2d046b0f3ebe2efc51b789934
James Cooper <jarak_jakar@hotmail.com>
2016-03-13 17:10:22 +1300

    First progress
    May have started to get parsing of CSV file working.  Maybe.

--- A2.cpp
+++ A2.cpp
@@ -4,7 +4,30 @@
 #include <armadillo>
 
 using namespace std;
-void main()
+using namespace arma;
+
+void main(int argc, char *argv[])
 {
-    cout << "Hello, world, from Visual C++!" << endl;
+    //cout << "Hello, world, from Visual C++!" << endl;
+    ifstream pointsFile;
+    pointsFile.open(argv[1]);
+    if(pointsFile.is_open()) {
+        string line;      
+        getline(pointsFile, line);
+        int totalLines = stoi(line);      
+        vector<vector<double>> inputPoints(totalLines);      
+        for(int i = 0; i < totalLines; i++) {
+            getline(pointsFile, line);
+            vector<double> tempArray;
+            for(int j = 0; j < 5; j++) {
+                string token = line.substr(0, line.find(","));
+                double tempVal = stod(token);
+                tempArray.push_back(tempVal);
+                line.erase(0, (line.find(",") + 1));
+            }
+            inputPoints.push_back(tempArray);
+        }
+        //cout << inputPoints.front().front() <<endl;
+    }
+    pointsFile.close();
 }
